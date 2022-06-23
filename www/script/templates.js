852cf4cecab22f1a1ad2ffd591144400eeac34a8
cubap <cubap@slu.edu>
2022-06-23 15:17:43 -0500

    missing s

--- www/script/templates.js
+++ www/script/templates.js
@@ -27,7 +27,7 @@ header.innerHTML = `
       <div class="search-icon" id="search-btn"><button title="Search" aria-label="Search" class="icon icon-search"></button></div>
       <div class="hamburger-icon"><div class="icon icon-menu">&nbsp;</div></div>
 </header>
-<link rel="stylesheet" href="https://centerfordigitalhumanities.github.io/Newberry-paleography/style/main.css">
+<link rel="stylesheet" href="https://centerfordigitalhumanities.github.io/Newberry-paleography/styles/main.css">
 <slot></slot>
 
 <nav class="container">
